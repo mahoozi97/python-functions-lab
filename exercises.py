404c0f17f79b92dbde2e03c0078b65ef67d8d12b
mahoozi97 <mahoozi97@icloud.com>
2026-05-02 11:23:33 +0300

    fix apply_discount function

--- exercises.py
+++ exercises.py
@@ -2,18 +2,21 @@ def calculate_area_triangle(base, height):
     area = (base * height) / 2
     return area
 
-print('Exercise 1:', calculate_area_triangle(10, 5))
+
+print("Exercise 1:", calculate_area_triangle(10, 5))
 
 
 def simple_interest(principal, rate, time):
     interest = (principal * rate * time) / 100
     return interest
 
+
 print("Exercise 2:", simple_interest(1000, 5, 2))
 
 
 def apply_discount(price, rate):
-    after_discount = price - rate / price * 100
+    # after_discount = price - (price * rate / 100) ✅
+    after_discount = price * (1 - rate / 100)
     return after_discount
 
 print("Exercise 3:", apply_discount(100, 25))
@@ -25,26 +28,30 @@ def convert_temperature(degree, type):
     elif type == "F":
         return f"{(degree - 32) * 5/9} C"
 
-print('Exercise 4: Convert 0°C to Fahrenheit:', convert_temperature(0, 'C'))
-print('Exercise 4: Convert 32°F to Celsius:', convert_temperature(32, 'F'))
+
+print("Exercise 4: Convert 0°C to Fahrenheit:", convert_temperature(0, "C"))
+print("Exercise 4: Convert 32°F to Celsius:", convert_temperature(32, "F"))
 
 
 def sum_to(n):
     return sum(range(1, n + 1))
 
-print('Exercise 5:', sum_to(6))
+
+print("Exercise 5:", sum_to(6))
 
 
 def largest(*number):
     return max(number)
 
-print('Exercise 6:', largest(8, 2, 3))
+
+print("Exercise 6:", largest(8, 2, 3))
 
 
 def calculate_tip(amount, percentage):
     return amount * (percentage / 100)
 
-print('Exercise 7:', calculate_tip(50, 20))
+
+print("Exercise 7:", calculate_tip(50, 20))
 
 
 def product(*numbers):
@@ -52,6 +59,7 @@ def product(*numbers):
     for n in numbers:
         result *= n
     return result
+
 
 print("Exercise 8:", product(2, 5, 5))
 print("Exercise 8:", product(1, -4))
@@ -66,5 +74,6 @@ def basic_calculator(num1, num2, operator):
         return num1 * num2
     elif operator == "divide":
         return num1 / num2
+
 
 print("Exercise 9 Result:", basic_calculator(10, 5, "divide"))
